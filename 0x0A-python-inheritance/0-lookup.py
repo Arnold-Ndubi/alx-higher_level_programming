682065dffdef4581dc0ef7613ccc5e1a39d34246
Arnold-Ndubi <ndubyarnold95@gmail.com>
2023-04-12 03:57:32 -0700

    Task o

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""defines a lookup function(object)."""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """gives a list of an objects that are present."""
+    """Return a list of an object's available attributes."""
     return (dir(obj))
