4f4d715f2edf5245d2346ec90101b417309e16b3
Arnold-Ndubi <ndubyarnold95@gmail.com>
2023-06-18 08:20:19 -0700

    task zero

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+
+"""Lists all ststes from database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
-        cur = db.cursor()
-
-        # Execute the query to fetch all states
-        cur.execute("SELECT * FROM states")
-        
-        # Fetch all the rows from the query result
-        rows = cur.fetchall()
-        
-        # Print the states
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-        sys.exit(1)
-    finally:
-        # Close the cursor and database connection
-        if cur:
-            cur.close()
-        if db:
-            db.close()
-
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
